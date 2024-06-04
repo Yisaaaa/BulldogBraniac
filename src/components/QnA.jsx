2064191d0dd094acfc67dcc59b21c603aceb4897
Yisaaaa <ryansanisit19@gmail.com>
2024-06-04 16:10:08 +0800

    ui update: put question on top of answer

--- src/components/QnA.jsx
+++ src/components/QnA.jsx
@@ -3,8 +3,8 @@ import React from "react";
 const QnA = (props) => {
   return (
     <div className="w-3/4 bg-white divide-y-4 border-2 rounded-md m-5">
-      <p className="px-5 py-2 text-lg font-medium">{props.answer}</p>
       <p className="px-5 py-2 text-lg font-medium">{props.question}</p>
+      <p className="px-5 py-2 text-lg font-medium">{props.answer}</p>
     </div>
   );
 };
